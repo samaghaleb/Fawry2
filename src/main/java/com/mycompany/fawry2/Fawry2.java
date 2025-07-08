ebe7f30961372263c4759b5d46c87c00e00b6fb8
samaghaleb <samaghaleb7@gmail.com>
2025-07-08 22:00:22 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/fawry2/Fawry2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fawry2;
+
+/**
+ *
+ * @author sama
+ */
+public class Fawry2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
